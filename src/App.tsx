a877f54f9efe79092f472a12ef5f0bbd4a431d3a
babisha <mdr.bebo@gmail.com>
2023-06-29 01:19:28 -0400

    Login Page and Register Page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
+import LoginPage from "./Pages/LoginPage";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <>
       <h1> Welcome </h1>
+      <LoginPage/>
     </>
   );
 }
