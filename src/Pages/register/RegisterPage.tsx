60738c683109ddb615cdcf592037e97ddea1baeb
Ashma Upreti <ashma.upreti99@gmail.com>
2023-06-30 17:19:22 -0400

    Completed form validation and page authorization on register and loginpage

--- src/Pages/register/RegisterPage.tsx
+++ src/Pages/register/RegisterPage.tsx
@@ -1,72 +1,200 @@
-import {React, useState} from 'react'
+import { useState } from "react";
 // import image from '../../images/login.jpg'
-import './style.css'
+import "./style.css";
+import { useNavigate } from "react-router-dom";
 
 function RegisterPage() {
-  const [userName, setUserName] = useState('')
-  const [fullName, setFullName] = useState('')
-  const [password, setPassword] = useState('')
-  const [password2, setPassword2] = useState('')
-  const [email, setEmail] = useState('')
+  const [userName, setUserName] = useState("");
+  const [fullName, setFullName] = useState("");
 
-  console.log(userName)
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [errorEmailMessage, setErrorEmailMessage] = useState("");
+  const [errorPasswordMessage, setErrorPasswordEmailMessage] = useState("");
+  console.log(userName);
+  const navigate = useNavigate();
 
-  return (<>
-  <div className='bg'>
-   <div className="grid grid-cols-2 bg-black/60 rounded-md">
-   {/* style = {{ backgroundImage: "url('../../images/login.jpg')" }} */}
-  <div className='flex'>
-		<form className="bg-slate-100/5 m-10 p-10 w-full rounded-md backdrop-blur-sm ">
-			<h1 className="flex justify-center text-slate-300 font-bold text-2xl mb-1 text-slate-100 mb-10">Create Account</h1>
-			<div className="flex items-center border-2 border-slate-100/10 hover:border-slate-100/20 py-2 px-3 rounded-md mb-4">
-				<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
-					fill="currentColor">
-					<path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-						clip-rule="evenodd" />
-				</svg>
-				<input value = {fullName} onChange={event=> setFullName(event.target.value)} className="pl-2 outline-none border-none bg-slate-100/0 w-max text-slate-300" type="text" name="fullname" id="fullname" placeholder="Full name" />
+  const HandleEmail = () => {
+    const regExp =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (email === "") {
+      // event.preventDefault();
+      setErrorEmailMessage("Fill the email field");
+    } else if (regExp.test(email)) {
+      setErrorEmailMessage("Email set");
+    } else if (!regExp.test(email)) {
+      setErrorEmailMessage("Enter valid email");
+    }
+  };
+  function HandlePassword() {
+    const regExp =
+      /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+    if (password === "" && password !== confirmPassword) {
+      setErrorPasswordEmailMessage("Please Enter Password");
+    } else if (regExp.test(password) && password === confirmPassword) {
+      navigate("/login", { state: { confirmPassword } });
+    } else if (!regExp.test(password)) {
+      setErrorPasswordEmailMessage("Please enter valid Password");
+    } else if (regExp.test(password) && password !== confirmPassword) {
+      setErrorPasswordEmailMessage("password didn't match");
+    }
+  }
+  function HandleRegister() {
+    HandleEmail();
+    HandlePassword();
+  }
+
+  return (
+    <>
+      <div className="bg">
+        <div className="grid grid-cols-2 bg-black/60 rounded-md">
+          {/* style = {{ backgroundImage: "url('../../images/login.jpg')" }} */}
+          <div className="flex">
+            <form className="bg-slate-100/5 m-10 p-10 w-full rounded-md backdrop-blur-sm border-2 ">
+              <h1 className="flex justify-center text-slate-300 font-bold text-2xl mb-1 text-slate-100 mb-10">
+                Create Account
+              </h1>
+              <div className="flex items-center border-2 border-slate-300 hover:border-slate-100 py-2 px-3 rounded-md mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-gray-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor">
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <input
+                  value={fullName}
+                  onChange={(event) => setFullName(event.target.value)}
+                  className="pl-2 outline-none border-none bg-slate-100/0 w-max text-slate-300"
+                  type="text"
+                  name="fullname"
+                  id="fullname"
+                  placeholder="Full name"
+                />
+              </div>
+              <div className="flex items-center border-2 border-slate-300 hover:border-slate-100 py-2 px-3 rounded-md mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-gray-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
+                  />
+                </svg>
+                <input
+                  value={userName}
+                  onChange={(event) => setUserName(event.target.value)}
+                  className="pl-2 outline-none border-none bg-slate-100/0 text-slate-300"
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="Username"
+                />
+              </div>
+              <div className="flex items-center border-2 border-slate-300 hover:border-slate-100 py-2 px-3 rounded-md mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-gray-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                  />
+                </svg>
+                <input
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                  className="pl-2 outline-none border-none bg-slate-100/0 text-slate-300"
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email Address"
+                />
+              </div>
+              {errorEmailMessage && (
+                <p className="text-red-500 mr-auto ">{errorEmailMessage}</p>
+              )}
+
+              <div className="flex items-center border-2 border-slate-300 hover:border-slate-100 py-2 px-3 rounded-md mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-gray-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor">
+                  <path
+                    fill-rule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <input
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                  className="pl-2 outline-none border-none bg-slate-100/0 text-slate-300"
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                />
+              </div>
+              {errorPasswordMessage && (
+                <p className="text-red-500 mr-auto ">{errorPasswordMessage}</p>
+              )}
+
+              <div className="flex mb-8 items-center border-2 border-slate-300 hover:border-slate-100 py-2 px-3 rounded-md mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-gray-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor">
+                  <path
+                    fill-rule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <input
+                  value={confirmPassword}
+                  onChange={(event) => setConfirmPassword(event.target.value)}
+                  className="pl-2 outline-none border-none bg-slate-100/0 text-slate-300"
+                  type="password"
+                  name="password2"
+                  id="password2"
+                  placeholder="Re-enter Password"
+                />
+              </div>
+              {errorPasswordMessage && (
+                <p className="text-red-500 mr-auto ">{errorPasswordMessage}</p>
+              )}
+
+              <button
+                onClick={HandleRegister}
+                className="block w-full mt-4 py-2 rounded-md text-slate-300 mb-10 font-semibold mb-2 bg-slate-100/5 hover:bg-slate-100/10">
+                Sign In
+              </button>
+              <span className="flex text-sm justify-center text-slate-400 ml-2 hover:text-slate-100 cursor-pointer">
+                Forgot Password ?
+              </span>
+            </form>
+          </div>
+        </div>
       </div>
-				<div className="flex items-center border-2 border-slate-100/10 hover:border-slate-100/20 py-2 px-3 rounded-md mb-4">
-					<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none"
-						viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-							d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
-					</svg>
-					<input value = {userName} onChange={event=> setUserName(event.target.value)} className="pl-2 outline-none border-none bg-slate-100/0 text-slate-300" type="text" name="username" id="username" placeholder="Username" />
-      </div>
-					<div className="flex items-center border-2 border-slate-100/10 hover:border-slate-100/20 py-2 px-3 rounded-md mb-4">
-						<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none"
-							viewBox="0 0 24 24" stroke="currentColor">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-								d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-						</svg>
-						<input value = {email} onChange={event=> setEmail(event.target.value)} className="pl-2 outline-none border-none bg-slate-100/0 text-slate-300" type="email" name="email" id="email" placeholder="Email Address" />
-      </div>
-						<div className="flex items-center border-2 border-slate-100/10 hover:border-slate-100/20 py-2 px-3 rounded-md mb-4">
-							<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
-								fill="currentColor">
-								<path fill-rule="evenodd"
-									d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-									clip-rule="evenodd" />
-							</svg>
-							<input value = {password} onChange={event=> setPassword(event.target.value)} className="pl-2 outline-none border-none bg-slate-100/0 text-slate-300" type="password" name="password" id="password" placeholder="Password" />
-      </div>
-            <div className="flex mb-8 items-center border-2 border-slate-100/10 hover:border-slate-100/20 py-2 px-3 rounded-md mb-4">
-							<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
-								fill="currentColor">
-								<path fill-rule="evenodd"
-									d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-									clip-rule="evenodd" />
-							</svg>
-							<input value = {password2} onChange={event=> setPassword2(event.target.value)} className="pl-2 outline-none border-none bg-slate-100/0 text-slate-300" type="password" name="password2" id="password2" placeholder="Re-enter Password" />
-      </div>
-							<button type="submit" className="block w-full mt-4 py-2 rounded-md text-slate-300 mb-10 font-semibold mb-2 bg-slate-100/5 hover:bg-slate-100/10">Login</button>
-							<span className="flex text-sm justify-center text-slate-400 ml-2 hover:text-slate-100 cursor-pointer">Forgot Password ?</span>
-		</form>
-</div>
-</div> 
-</div>
-    </>)
+    </>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
