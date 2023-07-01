341a538bbe8d92bb4f4a723b4f6697948b6a2365
Jharanatmg <jharanatmg22@gmail.com>
2023-06-30 22:08:07 -0700

    Create Home Page and Navbar

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 function HomePage() {
   return (
-    <div>
+    <div className='w-full h-full bg-[url("https://media.istockphoto.com/id/1252790348/photo/tengboche-monastery-roof-traditional-tibetan-buddhism-decoration-with-kangtega-6782m-mountain.jpg?s=612x612&w=0&k=20&c=3KDsWhZtrVDRL7ZxtJ_FqnY8gRskHUVIXKlvYZfbfOE=")]'>
+      <Navbar />
       <h1>WELCOME</h1>
 
     </div>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <ul className='w-4/5 h-32 flex inline gap-12'>
+            <li>Home</li>
+            <li>Trips</li>
+            <li>Contact Us</li>
+            <li>Learn More</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
