3864b79df9a6e42f67574e6869e88e7cf1c24055
Jharanatmg <jharanatmg22@gmail.com>
2023-06-30 23:19:30 -0700

    Implement Home Page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import logo from '../assets/Logo.png'
 function Navbar() {
+
   return (
     <div>
-        <ul className='w-4/5 h-32 flex inline gap-12'>
+        <ul className='w-4/5 h26 flex inline gap-32 justify-center items-center text-white'>
+            <li><img src={logo} alt="" srcset="" /></li>
             <li>Home</li>
             <li>Trips</li>
             <li>Contact Us</li>
