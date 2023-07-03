18696f8e5df2188506d35d897be6e0e390a0843e
Jharanatmg <jharanatmg22@gmail.com>
2023-07-03 12:58:21 -0700

    Add Trip Card

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+
 import logo from '../assets/Logo.png'
+import Card from './card'
+
 function Navbar() {
+  
 
   return (
     <div>
-        <ul className='w-4/5 h26 flex inline gap-32 justify-center items-center text-white'>
-            <li><img src={logo} alt="" srcset="" /></li>
-            <li>Home</li>
-            <li>Trips</li>
+        <ul className='w-4/5 h26 flex inline gap-16 justify-center items-center text-white p-5'>
+            <li><img src={logo} alt="" srcSet="" /></li>
+            
+           <li><Card /> </li>
+            <li>About Us</li>
             <li>Contact Us</li>
             <li>Learn More</li>
         </ul>
