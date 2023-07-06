ed5c27fd555a4bda9a9b6361ba914a1fdb2d2ed0
Ashma Upreti <ashma.upreti99@gmail.com>
2023-07-05 23:44:53 -0400

    create blog page

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import LoginPage from "./Pages/LoginPage";
 import { Routes, Route } from "react-router-dom";
 import RegisterPage from "./Pages/register/RegisterPage";
 import HomePage from "./Pages/HomePage";
+
 import { ModelContextProvider } from "./Context/ModelContext";
+import BlogPage from "./Pages/BlogPage";
 
 function App() {
   return (
     <ModelContextProvider>
       <Routes>
+        <Route path="/" element={<LoginPage />}></Route>
         <Route path="/Register" element={<RegisterPage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/HomePage" element={<HomePage />}></Route>
+        <Route path="/Homepage" element={<HomePage />}></Route>
+        <Route path="/Blogpage" element={<BlogPage />}></Route>
       </Routes>
     </ModelContextProvider>
   );
