0e212e30faf2f56c3542432293666efbef3d0b93
Sandesh Shrestha <sandes.sth@gmail.com>
2023-07-07 21:37:42 -0700

    Navbar issue

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import { useContext } from "react";
 import Footer from "../components/Footer";
-import NavBar from "../components/Navbar";
+import NavBar from "../components/navi-bar";
 import Card from "../components/card";
 import { ModelContext } from "../Context/ModelContext";
 function HomePage() {
@@ -12,48 +12,51 @@ function HomePage() {
   return (
     <>
       <div className="bg">
-      <div className="h-full bg-black/60">
-        <div className=" z-10 fixed top-0 left-0 right-0 z-index-100 w-full  ">
-          <NavBar />
-        </div>
-        <div className="p-24">
-        <main className="m-4 z-1 w-full h-full">
-          <div className="flex items-center justify-center flex-col w-full h-full">
-            <div className="bg-slate-100/5 w-96 h-52 rounded border border-slate-300/5 border-1 backdrop-blur flex flex-col items-center justify-center text-white">
-              <p className="text-xl">Welcome</p>
-              <p className="text-base">Lets Explore...</p>
-            </div>
+        <div className="h-full bg-black/60">
+          <div className=" z-10 fixed top-0 left-0 right-0 z-index-100 w-full  ">
+            <NavBar />
           </div>
-        </main>
-        <main className="m-4 z-1 w-full h-full">
-          <div className="flex items-center justify-center flex-col w-full h-full">
-            <div className="bg-slate-100/5 w-96 h-52 rounded border border-slate-300/5 border-1 backdrop-blur flex flex-col items-center justify-center text-white">
-              <p className="text-xl">Welcome</p>
-              <p className="text-base">Lets Explore...</p>
-            </div>
+          <div className="p-24">
+            <main className="m-4 z-1 w-full h-full">
+              <div className="flex items-center justify-center flex-col w-full h-full">
+                <div className="bg-slate-100/5 w-96 h-52 rounded border border-slate-300/5 border-1 backdrop-blur flex flex-col items-center justify-center text-white">
+                  <p className="text-xl">Welcome</p>
+                  <p className="text-base">Lets Explore...</p>
+                </div>
+              </div>
+            </main>
+            <main className="m-4 z-1 w-full h-full">
+              <div className="flex items-center justify-center flex-col w-full h-full">
+                <div className="bg-slate-100/5 w-96 h-52 rounded border border-slate-300/5 border-1 backdrop-blur flex flex-col items-center justify-center text-white">
+                  <p className="text-xl">Welcome</p>
+                  <p className="text-base">Lets Explore...</p>
+                </div>
+              </div>
+            </main>{" "}
+            <main className="m-4 z-1 w-full h-full">
+              <div className="flex items-center justify-center flex-col w-full h-full">
+                <div className="bg-slate-100/5 w-96 h-52 rounded border border-slate-300/5 border-1 backdrop-blur flex flex-col items-center justify-center text-white">
+                  <p className="text-xl">Welcome</p>
+                  <p className="text-base">Lets Explore...</p>
+                </div>
+              </div>
+            </main>
+            <main className="m-4 z-1 w-full h-full">
+              <div className="flex items-center justify-center flex-col w-full h-full">
+                <div className="bg-slate-100/5 w-96 h-52 rounded border border-slate-300/5 border-1 backdrop-blur flex flex-col items-center justify-center text-white">
+                  <p className="text-xl">Welcome</p>
+                  <p className="text-base">Lets Explore...</p>
+                </div>
+              </div>
+            </main>
           </div>
-        </main> <main className="m-4 z-1 w-full h-full">
-          <div className="flex items-center justify-center flex-col w-full h-full">
-            <div className="bg-slate-100/5 w-96 h-52 rounded border border-slate-300/5 border-1 backdrop-blur flex flex-col items-center justify-center text-white">
-              <p className="text-xl">Welcome</p>
-              <p className="text-base">Lets Explore...</p>
-            </div>
-          </div>
-        </main><main className="m-4 z-1 w-full h-full">
-          <div className="flex items-center justify-center flex-col w-full h-full">
-            <div className="bg-slate-100/5 w-96 h-52 rounded border border-slate-300/5 border-1 backdrop-blur flex flex-col items-center justify-center text-white">
-              <p className="text-xl">Welcome</p>
-              <p className="text-base">Lets Explore...</p>
-            </div>
-          </div>
-        </main>
-        </div>
 
-        <div  >
-          <Footer />
+          <div>
+            <Footer />
+          </div>
+          {status && <Card />}
         </div>
-        {status && <Card />}
-      </div></div>
+      </div>
     </>
   );
 }
--- src/components/navi-bar.tsx
+++ src/components/navi-bar.tsx
@@ -53,25 +53,25 @@ function NavBar() {
                   </a>
                   <a
                     href="/blogpage"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    className="text-gray-300 hover:bg-gray-300/10 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                   >
                     Blog
                   </a>
                   <a
                     href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    className="text-gray-300 hover:bg-gray-300/10 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                   >
                     Activities
                   </a>
                   <a
                     href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    className="text-gray-300 hover:bg-gray-300/10 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                   >
                     Destinations
                   </a>
                   <a
                     href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    className="text-gray-300 hover:bg-gray-300/10 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                   >
                     Calendar
                   </a>
